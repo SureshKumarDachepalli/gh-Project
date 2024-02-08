d2be13fd8ae513e23ee9e6bc58914c958f7ad458
dell <dell@192.168.1.19>
2024-02-08 15:33:28 +0530

    ghproject local repository message

---
+++ src/main/java/gu/A.java
@@ -0,0 +1,10 @@
+package gu;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println(" i have to create one class src/main/java folder");
+
+	}
+
+}
---
+++ src/test/java/B.java
@@ -0,0 +1,10 @@
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" i have to create SECOND class in src/test/java folder");
+
+	}
+
+}
